000cedbb89b54074dbeac9687f60f6d9b125b9a3
Thomas F. K. Jorna <thomasfkjorna@gmail.com>
2021-07-30 22:02:52 +0200

    removed notworking slider customization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -175,12 +175,6 @@ function SubApp(props: any) {
             },
           },
         },
-        SliderThumb: {
-          bg: highlightColor + '.500',
-        },
-        SliderFilledTrack: {
-          bg: 'gray.400',
-        },
       },
     }
   }, [highlightColor, JSON.stringify(emacsTheme)])
