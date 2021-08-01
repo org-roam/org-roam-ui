361e20367c20e4bae6a02391a6852bb345fadc9a
Thomas F. K. Jorna <thomasfkjorna@gmail.com>
2021-08-01 15:37:32 +0200

    removed logs, maybe included complete build

---
+++ out/_next/static/ZRG0ryTs3ibecuFrzNLgh/_buildManifest.js
@@ -0,0 +1,12 @@
+;(self.__BUILD_MANIFEST = {
+  __rewrites: { beforeFiles: [], afterFiles: [], fallback: [] },
+  '/': [
+    'static/chunks/fb7d5399-0d6001c72a29ebec41eb.js',
+    'static/chunks/d25bd147-65fcc4c92edba8b370fb.js',
+    'static/chunks/906-7b9696c9b17c64b94384.js',
+    'static/chunks/pages/index-af70b77619b241a63065.js',
+  ],
+  '/_error': ['static/chunks/pages/_error-a0e21b9b223f827fe1f2.js'],
+  sortedPages: ['/', '/_app', '/_error'],
+}),
+  self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
