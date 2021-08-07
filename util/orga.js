872be7c3631895d951e1991b0bcbd4d164941dab
Thomas F. K. Jorna <thomasfkjorna@gmail.com>
2021-08-07 13:26:48 +0200

    chore: remove test files

--- util/orga.js
+++
@@ -1,8 +0,0 @@
-const stream = import('unified-stream')
-const reorg = require('@orgajs/reorg')
-const mutate = require('@orgajs/reorg-rehype')
-const html = import('rehype-stringify')
-
-const processor = reorg().use(mutate).use(html)
-
-process.stid.pipe(stream(processor)).pipe(process.stdout)
