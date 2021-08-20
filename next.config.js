a62d08c2ab525cb90ff1bb9d061cfd00c6180473
Thomas F. K. Jorna <thomasfkjorna@gmail.com>
2021-08-20 19:32:42 +0200

    fix: remove service worker. Possibly fixes #61

--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
 const fs = require('fs')
 
 const withPlugins = require('next-compose-plugins')
-const withPWA = require('next-pwa')
-const withOrga = require('@orgajs/next')
 
 const d3packages = fs.readdirSync('node_modules').filter((name) => name.startsWith('d3-'))
 const withTM = require('next-transpile-modules')(d3packages)
 
-module.exports = withPlugins([withTM, withPWA, withOrga], {
+module.exports = withPlugins([withTM], {
   distDir: 'build',
-  pwa: {
-    dest: 'build',
-  },
 })
