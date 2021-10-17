ca5cfb62c1cc1e2850103ba13ede59380e6808ba
Thomas F. K. Jorna <hello@tefkah.com>
2021-10-18 00:52:21 +0200

    feat: exclude nodes from local graph (#115)

---
+++ components/Sidebar/Title.tsx
@@ -0,0 +1,25 @@
+import { Flex, Heading } from '@chakra-ui/react'
+import React from 'react'
+import { BiFile } from 'react-icons/bi'
+
+import { OrgRoamNode } from '../../api'
+export interface TitleProps {
+  previewNode: OrgRoamNode
+}
+
+export const Title = (props: TitleProps) => {
+  const { previewNode } = props
+  return (
+    <Flex maxW="90%">
+      {/* <BiFile
+       * // onContextMenu={(e) => {
+       * //   e.preventDefault()
+       * //   openContextMenu(previewNode, e)
+       * // }}
+       * /> */}
+      <Heading lineHeight={1.2} size="md" fontWeight={600} pt={4}>
+        {previewNode?.title}
+      </Heading>
+    </Flex>
+  )
+}
