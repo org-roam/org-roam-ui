acff639a4e8a383e512d093edbcbcac69549c3cb
Thomas F. K. Jorna <hello@tefkah.com>
2021-11-12 20:22:58 +0100

    feat(md): initial support for md-roam

--- util/processOrg.tsx
+++ util/processOrg.tsx
@@ -12,16 +12,22 @@ import katex from 'rehype-katex'
 import 'katex/dist/katex.css'
 import rehype2react from 'rehype-react'
 
+import remarkParse from 'remark-parse'
+import remarkGFM from 'remark-gfm'
+import remarkMath from 'remark-math'
+import remarkRehype from 'remark-rehype'
+
 import { PreviewLink } from '../components/Sidebar/Link'
 import { NodeByCite, NodeById } from '../pages'
 import React, { createContext, ReactNode, useMemo } from 'react'
 import { OrgImage } from '../components/Sidebar/OrgImage'
 import { Section } from '../components/Sidebar/Section'
 import { NoteContext } from './NoteContext'
+import { OrgRoamNode } from '../api'
 
 export interface ProcessedOrgProps {
   nodeById: NodeById
-  previewNode: any
+  previewNode: OrgRoamNode
   setPreviewNode: any
   previewText: any
   nodeByCite: NodeByCite
@@ -44,47 +50,51 @@ export const ProcessedOrg = (props: ProcessedOrgProps) => {
     collapse,
   } = props
 
-  const processor = unified()
+  const orgProcessor = unified()
     .use(uniorgParse)
     .use(extractKeywords)
     .use(attachments)
     .use(uniorgSlug)
     .use(uniorg2rehype, { useSections: true })
-    //.data('settings', { fragment: true })
-    // .use(highlight)
-    .use(katex)
-    .use(rehype2react, {
-      createElement: React.createElement,
-      // eslint-disable-next-line react/display-name
-      components: {
-        a: ({ children, href }) => {
-          return (
-            <PreviewLink
-              nodeByCite={nodeByCite}
-              setSidebarHighlightedNode={setSidebarHighlightedNode}
-              href={`${href as string}`}
-              nodeById={nodeById}
-              setPreviewNode={setPreviewNode}
-              openContextMenu={openContextMenu}
-              outline={outline}
-            >
-              {children}
-            </PreviewLink>
-          )
-        },
-        img: ({ src }) => {
-          return <OrgImage src={src as string} file={previewNode?.file} />
-        },
-        section: ({ children, className }) => (
-          <Section {...{ outline, collapse }} className={className as string}>
+
+  const mdProcessor = unified().use(remarkParse).use(remarkMath).use(remarkGFM).use(remarkRehype)
+  //.data('settings', { fragment: true })
+  // .use(highlight)
+
+  const baseProcessor = previewNode?.file?.slice(-3) === '.md' ? mdProcessor : orgProcessor
+
+  const processor = baseProcessor.use(katex).use(rehype2react, {
+    createElement: React.createElement,
+    // eslint-disable-next-line react/display-name
+    components: {
+      a: ({ children, href }) => {
+        return (
+          <PreviewLink
+            nodeByCite={nodeByCite}
+            setSidebarHighlightedNode={setSidebarHighlightedNode}
+            href={`${href as string}`}
+            nodeById={nodeById}
+            setPreviewNode={setPreviewNode}
+            openContextMenu={openContextMenu}
+            outline={outline}
+          >
             {children}
-          </Section>
-        ),
-        p: ({ children }) => {
-          return <p lang="en">{children as ReactNode}</p>
-        },
+          </PreviewLink>
+        )
       },
-    })
+      img: ({ src }) => {
+        return <OrgImage src={src as string} file={previewNode?.file} />
+      },
+      section: ({ children, className }) => (
+        <Section {...{ outline, collapse }} className={className as string}>
+          {children}
+        </Section>
+      ),
+      p: ({ children }) => {
+        return <p lang="en">{children as ReactNode}</p>
+      },
+    },
+  })
 
   const text = useMemo(() => processor.processSync(previewText).result, [previewText])
   return (
